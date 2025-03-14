517076d74c1a161330d8af49dcfc4eafbce83db9
Harsha <lekamge.h2024@gmail.com>
2025-03-14 23:59:44 +0530

    Update App.js
    change PSR to Harsha

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>welcome to PSR vlog</h1>
+        <h1>welcome to Harsha Vlog</h1>
         <button className='users-button'>Users</button>
         </header>
     </div>
